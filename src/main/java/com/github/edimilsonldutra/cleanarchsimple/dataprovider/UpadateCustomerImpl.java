8e5498ba202691f8f071ed57b419253a3176ecdb
Edimilson de Lima <edimilsonldutra@hotmail.com>
2023-02-20 18:15:20 -0300

    incluir metodo update no controller

---
+++ src/main/java/com/github/edimilsonldutra/cleanarchsimple/dataprovider/UpadateCustomerImpl.java
@@ -0,0 +1,23 @@
+package com.github.edimilsonldutra.cleanarchsimple.dataprovider;
+
+import com.github.edimilsonldutra.cleanarchsimple.core.dataprovider.UpdateCustomer;
+import com.github.edimilsonldutra.cleanarchsimple.core.domain.Customer;
+import com.github.edimilsonldutra.cleanarchsimple.dataprovider.repository.CustomerRepository;
+import com.github.edimilsonldutra.cleanarchsimple.dataprovider.repository.mapper.CustomerEntityMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpadateCustomerImpl implements UpdateCustomer {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void update(Customer customer) {
+        var customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+}
--- src/main/java/com/github/edimilsonldutra/cleanarchsimple/entrypoint/controller/CustomerController.java
+++ src/main/java/com/github/edimilsonldutra/cleanarchsimple/entrypoint/controller/CustomerController.java
@@ -2,10 +2,12 @@ package com.github.edimilsonldutra.cleanarchsimple.entrypoint.controller;
 
 import com.github.edimilsonldutra.cleanarchsimple.core.usecase.FindCustomerByIdUseCase;
 import com.github.edimilsonldutra.cleanarchsimple.core.usecase.InsertCustomerUseCase;
+import com.github.edimilsonldutra.cleanarchsimple.core.usecase.UpdateCustomerUseCase;
 import com.github.edimilsonldutra.cleanarchsimple.entrypoint.controller.mapper.CustomerMapper;
 import com.github.edimilsonldutra.cleanarchsimple.entrypoint.controller.request.CustomerResquest;
 import com.github.edimilsonldutra.cleanarchsimple.entrypoint.controller.response.CustomerResponse;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,8 @@ public class CustomerController {
     private InsertCustomerUseCase insertCustomerUseCase;
     private FindCustomerByIdUseCase findCustomerByIdUseCase;
     private CustomerMapper customerMapper;
+    private UpdateCustomerUseCase updateCustomerUseCase;
+
     @PostMapping
     public ResponseEntity<Void> insert(@Valid @RequestBody CustomerResquest customerResquest) {
         var customer = customerMapper.toCustomer(customerResquest);
@@ -30,4 +34,12 @@ public class CustomerController {
         return ResponseEntity.ok().body(customerResponse);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable final String id,
+                                       @Valid @RequestBody CustomerResquest customerResquest) {
+        var customer = customerMapper.toCustomer(customerResquest);
+        customer.setId(id);
+        updateCustomerUseCase.update(customer, customerResquest.getZipCode());
+        return ResponseEntity.noContent().build();
+    }
 }
